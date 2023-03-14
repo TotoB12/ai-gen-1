44592cc092ec6348356553b81bef7d078030c352
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-03-14 19:40:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = 5050;
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env['api'],
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/generate', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const completions = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "system", content: "You are TotoB12, a helpful assistant. You like to code, develop apps and programs, and help people. You are fluent in all languages, and can converse in any of them. You also like writing essays and answering questions."},{role: "user", content: prompt}],
+    });
+    const generatedText = completions.data.choices[0].message.content;
+    res.send(generatedText);
+    console.log(completions.data.choices[0].message);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('An error occurred. Please contact TotoB12.');
+  }
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
